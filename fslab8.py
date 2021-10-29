da49556dbc52b862aecaf4855e5d836f3a5b4e23
Jennifer Sager <jennysager2194@gmail.com>
2021-10-28 23:32:15 -0400

    Create fslab8.py

---
+++ fslab8.py
@@ -0,0 +1,46 @@
+#1 and 2
+first = "Jenny"
+last = "SAGER"
+
+#3 and 4
+print(first.upper(), last.lower())
+print("\n", "\n")
+
+#5 and 6
+fullname = "Jenny Sager"
+print(fullname[6:11])
+
+#7
+fullname = "Jenny Sager, Wayne State Student"
+print(fullname)
+
+#8
+print("\"Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible - Francis of Assisi\"")
+
+#9
+num1 = 9.5
+num2 = 5.9
+
+#10
+add = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+
+#11
+print(num1, "plus", num2, "equals", add)
+print(str(num1) + " minus " + str(num2) + " equals " + str(sub))
+print("%d multiplied by %d equals %d" % (num1, num2, mult))
+print("{} divided by {} equals {}".format(num1, num2, div))
+
+#12
+sq_root = mult ** 0.5
+roundednum = round(sq_root, 2)
+print("The square root of", round(mult, 2), "equals", roundednum)
+
+#13 and 14
+current_month = "October"
+dayofmonth = 28
+print("\t\tToday is day " + str(dayofmonth) + " of the month " + current_month)
+
+
